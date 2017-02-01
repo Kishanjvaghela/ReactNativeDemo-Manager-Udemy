19809c89bca01d915af80af275db5466f695ebbb
Kishan Vaghela <kishan.j.vaghela@gmail.com>
2017-02-01 12:44:01 +0530

    bind redux for password changes

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED
+} from './types';
+
+export const emailChange = (text) => {
+  return {
+    type: EMAIL_CHANGED,
+    payload: text
+  };
+};
+
+export const passwordChanged = (text) => {
+  return {
+    type: PASSWORD_CHANGED,
+    playload: text
+  };
+};
---
+++ src/reducers/AuthReducer.js
@@ -0,0 +1,20 @@
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED
+} from '../actions/types';
+
+const INITIAL_STATE = {
+  email: '' ,
+  password: ''
+};
+
+export default (state = INITIAL_STATE, actions) => {
+  switch (actions.type) {
+    case EMAIL_CHANGED:
+      return { ...state, email: actions.playload };
+    case PASSWORD_CHANGED:
+        return { ...state, password: actions.playload };
+    default:
+      return state;
+  }
+};
