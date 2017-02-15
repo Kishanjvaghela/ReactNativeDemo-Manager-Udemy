e530e9d439d50508d15566ef386c299c87c41d7d
Kishan Vaghela <kishan.j.vaghela@gmail.com>
2017-02-15 16:21:03 +0530

    save employee to firebase

--- src/actions/EmployeeAction.js
+++ src/actions/EmployeeAction.js
@@ -4,7 +4,8 @@ import { Actions } from 'react-native-router-flux';
 import {
   EMPLOYEE_UPDATE,
   EMPLOYEE_CREATE,
-  EMPLOYEE_FETCH_SUCCESS
+  EMPLOYEE_FETCH_SUCCESS,
+  EMPLOYEE_SAVE_SUCCESS
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -35,4 +36,18 @@ export const employeeFetch = () => {
           dispatch({ type : EMPLOYEE_FETCH_SUCCESS, payload: snapshot.val() });
       });
   };
+};
+
+export const employeeSave = ({ name, phone, shift, uid }) => {
+  const { currentUser } = firebase.auth();
+  return (dispatch) => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
+      .set( { name, phone, shift })
+      .then( () => {
+        dispatch({ type: EMPLOYEE_SAVE_SUCCESS });
+        Actions.employeeList( { type: 'reset'});
+      });
+
+  }
+
 };
--- src/actions/types.js
+++ src/actions/types.js
@@ -6,3 +6,4 @@ export const LOGIN_USER_FAILED = 'login_failed';
 export const EMPLOYEE_UPDATE = 'employee_update';
 export const EMPLOYEE_CREATE = "employee_create";
 export const EMPLOYEE_FETCH_SUCCESS = 'employee_fetch';
+export const EMPLOYEE_SAVE_SUCCESS = 'employee_save_success';
